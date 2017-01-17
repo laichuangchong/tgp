43993608dde96e534d6246cf06abc415ae8819ee
陈仲英 <chenzhongying@chenzhongyingdeiMac.local>
2017-01-17 15:26:36 +0800

    添加版本好

--- js/common.js
+++ js/common.js
@@ -6,211 +6,211 @@ tgp.config(function ($routeProvider) {
     $routeProvider
         .when('/',
             {
-                templateUrl: 'find/lostEmpire.html',
+                templateUrl: 'find/lostEmpire.html?v=1.0',
                 controller: 'lostEmpire'
             }
         )
         .when('/find', /*发现*/
             {
-                templateUrl: 'find/lostEmpire.html',
+                templateUrl: 'find/lostEmpire.html?v=1.0',
                 controller: 'lostEmpire'
             }
         )
         .when('/lostEmpire', /*种族*/
             {
-                templateUrl: 'find/lostEmpire.html',
+                templateUrl: 'find/lostEmpire.html?v=1.0',
                 controller: 'lostEmpire'
             }
         )
         .when('/thought',  /*思潮*/
             {
-                templateUrl: 'find/thought.html',
+                templateUrl: 'find/thought.html?v=1.0',
                 controller: 'thought'
             }
         )
         .when('/speciality',  /*特质与基因*/
             {
-                templateUrl: 'find/speciality.html',
+                templateUrl: 'find/speciality.html?v=1.0',
                 controller: 'speciality'
             }
         )
         .when('/universe',  /*宇宙探索*/
             {
-                templateUrl: 'find/universe.html',
+                templateUrl: 'find/universe.html?v=1.0',
                 controller: 'universe'
             }
         )
         .when('/ftl',  /*FTL*/
             {
-                templateUrl: 'find/ftl.html',
+                templateUrl: 'find/ftl.html?v=1.0',
                 controller: 'ftl'
             }
         )
         .when('/abnormal',  /*异常*/
             {
-                templateUrl: 'find/abnormal.html',
+                templateUrl: 'find/abnormal.html?v=1.0',
                 controller: 'abnormal'
             }
         )
         .when('/celestial',  /*天体*/
             {
-                templateUrl: 'find/celestial.html',
+                templateUrl: 'find/celestial.html?v=1.0',
                 controller: 'celestial'
             }
         )
         .when('/lastday',  /*太空生物*/
             {
-                templateUrl: 'find/lastday.html',
+                templateUrl: 'find/lastday.html?v=1.0',
                 controller: 'lastday'
             }
         )
         .when('/manage', /*管理*/
             {
-                templateUrl: 'manage/government.html',
+                templateUrl: 'manage/government.html?v=1.0',
                 controller: 'government'
             }
         )
         .when('/government', /*政府*/
             {
-                templateUrl: 'manage/government.html',
+                templateUrl: 'manage/government.html?v=1.0',
                 controller: 'government'
             }
         )
         .when('/policy', /*政策*/
             {
-                templateUrl: 'manage/policy.html',
+                templateUrl: 'manage/policy.html?v=1.0',
                 controller: 'policy'
             }
         )
         .when('/act', /*法令*/
             {
-                templateUrl: 'manage/act.html',
+                templateUrl: 'manage/act.html?v=1.0',
                 controller: 'act'
             }
         )
         .when('/leader', /*领袖*/
             {
-                templateUrl: 'manage/leader.html',
+                templateUrl: 'manage/leader.html?v=1.0',
                 controller: 'leader'
             }
         )
         .when('/factions', /*派系*/
             {
-                templateUrl: 'manage/factions.html',
+                templateUrl: 'manage/factions.html?v=1.0',
                 controller: 'factions'
             }
         )
         .when('/population', /*人口*/
             {
-                templateUrl: 'manage/population.html',
+                templateUrl: 'manage/population.html?v=1.0',
                 controller: 'population'
             }
         )
         .when('/military', /*军事*/
             {
-                templateUrl: 'military/war.html',
+                templateUrl: 'military/war.html?v=1.0',
                 controller: 'war'
             }
         )
         .when('/war', /*战争*/
             {
-                templateUrl: 'military/war.html',
+                templateUrl: 'military/war.html?v=1.0',
                 controller: 'war'
             }
         )
         .when('/spaceBattle', /*太空战斗*/
             {
-                templateUrl: 'military/spaceBattle.html',
+                templateUrl: 'military/spaceBattle.html?v=1.0',
                 controller: 'spaceBattle'
             }
         )
         .when('/groundBattle', /*地面战斗*/
             {
-                templateUrl: 'military/groundBattle.html',
+                templateUrl: 'military/groundBattle.html?v=1.0',
                 controller: 'groundBattle'
             }
         )
         .when('/progress', /*进阶攻略*/
             {
-                templateUrl: 'progress/progress.html',
+                templateUrl: 'progress/progress.html?v=1.0',
                 controller: 'progress'
             }
         )
         .when('/beginner', /*新手攻略*/
             {
-                templateUrl: 'beginner/empireChoice.html',
+                templateUrl: 'beginner/empireChoice.html?v=1.0',
                 controller: 'gameOpen'
             }
         )
         .when('/gameOpen', /*游戏开局*/
             {
-                templateUrl: 'beginner/gameOpen.html',
+                templateUrl: 'beginner/gameOpen.html?v=1.0',
                 controller: 'gameOpen'
             }
         )
         .when('/empireChoice', /*帝国选择*/
             {
-                templateUrl: 'beginner/empireChoice.html',
+                templateUrl: 'beginner/empireChoice.html?v=1.0',
                 controller: 'empireChoice'
             }
         )
         .when('/initialEconomic', /*初期经济*/
             {
-                templateUrl: 'beginner/initialEconomic.html',
+                templateUrl: 'beginner/initialEconomic.html?v=1.0',
                 controller: 'initialEconomic'
             }
         )
         .when('/ethnic', /*种族交流*/
             {
-                templateUrl: 'beginner/ethnic.html',
+                templateUrl: 'beginner/ethnic.html?v=1.0',
                 controller: 'ethnic'
             }
         )
         .when('/threats', /*早期威胁与战争*/
             {
-                templateUrl: 'beginner/threats.html',
+                templateUrl: 'beginner/threats.html?v=1.0',
                 controller: 'threats'
             }
         )
         .when('/diplomacy', /*外交*/
             {
-                templateUrl: 'diplomacy/routine.html',
+                templateUrl: 'diplomacy/routine.html?v=1.0',
                 controller: 'routine'
             }
         )
         .when('/routine', /*常规外交*/
             {
-                templateUrl: 'diplomacy/routine.html',
+                templateUrl: 'diplomacy/routine.html?v=1.0',
                 controller: 'routine'
             }
         )
         .when('/subsidiary', /*附庸国*/
             {
-                templateUrl: 'diplomacy/subsidiary.html',
+                templateUrl: 'diplomacy/subsidiary.html?v=1.0',
                 controller: 'subsidiary'
             }
         )
         .when('/allianceAgreement', /*联盟协定*/
             {
-                templateUrl: 'diplomacy/allianceAgreement.html',
+                templateUrl: 'diplomacy/allianceAgreement.html?v=1.0',
                 controller: 'allianceAgreement'
             }
         )
         .when('/trade', /*贸易*/
             {
-                templateUrl: 'diplomacy/trade.html',
+                templateUrl: 'diplomacy/trade.html?v=1.0',
                 controller: 'trade'
             }
         )
         .when('/ai', /*AI帝国性格*/
             {
-                templateUrl: 'diplomacy/ai.html',
+                templateUrl: 'diplomacy/ai.html?v=1.0',
                 controller: 'ai'
             }
         )
         .when('/raidersVideo', /*攻略视频*/
             {
-                templateUrl: 'raidersVideo/raidersVideo.html',
+                templateUrl: 'raidersVideo/raidersVideo.html?v=1.0',
                 controller: 'raidersVideo'
             }
         )
